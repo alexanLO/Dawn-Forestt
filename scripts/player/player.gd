5a3b43fed2ef5a55fd3a56eabda941cf31609a2a
Alexan Lima <alexanlima1.0@gmail.com>
2023-09-21 11:56:07 -0300

    Script Inicial do Personagem | Movimentação Horizontal.

---
+++ scripts/player/player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+class_name Player
+
+onready var player_sprite: Sprite = get_node("Texture")
+
+var velocity: Vector2 #Ele guarda dois valores (x, y)
+
+export(int) var speed
+
+#Godot recomenda usar essa funcão para objetos que usam a física. Funções proprias da godot não precisa especificar
+#se é void ou não.
+func _physics_process(delta: float):
+	horizontal_moviment_env()
+	#Esse é um metódo do KinematicBody que aplica uma velocidade linear ao objeto e apartir disso ele vai movimentar 
+	#o personagem, a vantagem é que além de movimentar o personagem, ele vai slidar. Slide faz com que ele se movimente
+	#e quando ele encontrar uma colisão dependendo dos parâmetros ele vai executar determinadas ações, se não 
+	#especificar um parâmetro quando ele encontrar uma colisão ele vai parar.
+	velocity = move_and_slide(velocity)
+	
+func horizontal_moviment_env() -> void:
+	#Vai retorna qual é a direção. Se for pra esquerda é -1, se for para a direita é 1 e se pressionar
+	#os dois ou nenhum vai ser 0.
+	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = input_direction * speed
+	
