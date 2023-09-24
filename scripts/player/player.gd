63373cee1035722028f8b830c5a5f958fda219b6
Alexan Lima <alexanlima1.0@gmail.com>
2023-09-24 16:04:59 -0300

    Código para Aplicar a Gravidade | Pulo na Parede.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,22 +2,29 @@ extends KinematicBody2D
 class_name Player
 
 onready var player_sprite: Sprite = get_node("Texture")
+onready var wall_ray: RayCast2D = get_node("wallRay")
 
 var velocity: Vector2 #Ele guarda dois valores (x, y)
 var jump_count: int = 0
 
 var landing: bool = false
+var on_wall: bool = false
 var attacking: bool = false
 var defending: bool = false
 var crouching: bool = false
+var direction: int = 1
 
 #Essa variável vai auxiliar no código para quando ela estiver na ação de attack, defese ou croach o personagem não
 #vai poder pular e movimentação horizontal.
 var can_track_input: bool = true 
+var not_on_wall: bool = true
 
 export(int) var jump_speed
-export(int) var player_gravity
 export(int) var speed
+export(int) var wall_jump_speed
+export(int) var player_gravity
+export(int) var wall_gravity
+export(int) var wall_impulse_speed
 
 #Godot recomenda usar essa funcão para objetos que usam a física. Funções proprias da godot não precisa especificar
 #se é void ou não.
@@ -40,19 +47,25 @@ func horizontal_moviment_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	if can_track_input == false or attacking:
 		velocity.x = 0
+		return
+		
 	velocity.x = input_direction * speed
 
 func vertical_moviment_env() -> void:
 	#Essa condição é necessária para resetar os pulos e a pessoa poder da mais 2 pulos, se essa condição
 	#for colocada depois do ins_action_just... ele vai passar a dar 3 pulos.
-	if is_on_floor():
+	if is_on_floor() or is_on_wall():
 		jump_count = 0
 	#Essa condição faz o jogador das 2 pulos e ele analisa se a pessoa aperta o espaço e segurar ou
 	#aperta rapidamente ele faça apenas uma vez a ação.
 	var jump_condition = can_track_input and not attacking
 	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and jump_condition:
 		jump_count += 1
-		velocity.y = jump_speed
+		if next_to_wall() and not is_on_floor():
+			velocity.y = wall_jump_speed
+			velocity.x = wall_impulse_speed * direction
+		else:
+			velocity.y = jump_speed
 
 func actions_env() -> void:
 	attack()
@@ -87,7 +100,27 @@ func crouch() -> void:
 
 #Aplicando a velocidade em y, delta e valor de gravidade a cada frame.
 func gravity(delta: float) -> void:
-	velocity.y += delta*player_gravity
-	#Limitador de velocidade de queda na vertical.
-	if velocity.y >= player_gravity:
-		velocity.y = player_gravity
+	if next_to_wall() :
+		velocity.y += delta * wall_gravity
+		if velocity.y >= wall_gravity:
+			velocity = wall_gravity
+	else:
+		velocity.y += delta*player_gravity
+		#Limitador de velocidade de queda na vertical.
+		if velocity.y >= player_gravity:
+			velocity.y = player_gravity
+
+func next_to_wall() -> bool:
+	if wall_ray.is_colliding() and not is_on_floor():
+		#Esse condição vai fazer com que quando o personagem pule na parede ele não comece ja deslizar para baixo
+		#ele começa a deslizar para cima porque a força de pulo da vertical pra cima é muito maior que a gravidade pra baixo.
+		if not_on_wall:
+			#Se ele não estava perto da parede posteriormente, então ele vai zerar a velocidade em Y pois dessa forma 
+			#quando o personagem colidir com a parede ele não vai aplicar essa força para cima já que a velocidade 
+			#está zerada em Y. Assim aplicando o deslizamento para baixo.
+			velocity.y = 0
+			not_on_wall = false
+		return true
+	else:
+		not_on_wall = true
+		return false
