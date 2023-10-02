f5048049fafa7d6fe2ea2e58c47c28a2b8bbc363
Alexan Lima <alexanlima1.0@gmail.com>
2023-10-02 14:53:44 -0300

    Script de Movimentação Horizontal.

---
+++ scripts/enemy/enemyTemplate.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+class_name EnemyTemplate
+
+onready var texture: Sprite = get_node("Texture")
+onready var floor_ray: RayCast2D = get_node("FloorRay")
+onready var animation: AnimationPlayer = get_node("Animation")
+
+var can_die: bool = false
+var can_hit: bool = false
+var can_attack: bool = false
+
+var velocity: Vector2
+var player_ref: Player = null
+
+export(int) var speed
+export(int) var gravity_speed
+export(int) var proximity_threshold #Essa variavel vai ajuda ao inimigo parar de perseguir o player e fazer o attack.
+
+func _physics_process(delta: float) -> void:
+	gravity(delta)
+	move_behavior()
+
+func gravity(delta: float) -> void:
+	velocity.y = delta * gravity_speed
+
+func move_behavior() -> void:
+	if player_ref != null:
+		var distance: Vector2 = player_ref.global_position - global_position
+		var direction: Vector2 = distance.normalized()
+		if abs(distance.x) <= proximity_threshold:
+			velocity.x = 0 #Para não atacar andando
+			can_attack = true
+		elif floor_collision() and not can_attack:
+			velocity.x = direction.x * speed
+		else:
+			velocity.x = 0
+		
+		return #O return é para não executar nada que estiver embaixo dele.
+		
+	velocity.x = 0
+
+func floor_collision() -> bool:
+	if floor_ray.is_colliding():
+		return true
+		
+	return false
