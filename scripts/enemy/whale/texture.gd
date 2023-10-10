fb04da5344936d991cbea1fa9055b585cfb069dd
Alexan Lima <alexanlima1.0@gmail.com>
2023-10-10 13:22:25 -0300

    Configurando as Propriedades e Colisões da Whale.

---
+++ scripts/enemy/whale/texture.gd
@@ -0,0 +1,14 @@
+extends EnemyTexture #Isso não da problema porque EnemyTexture herda de Sprite que é do mesmo tipo.
+class_name WhaleTexture
+
+func animate(velocity: Vector2) -> void:
+	move_behavior(velocity)
+
+func move_behavior(velocity: Vector2) -> void:
+	if velocity.x != 0:
+		animation.play("run")
+	else:
+		animation.play("idle")
+
+
+
