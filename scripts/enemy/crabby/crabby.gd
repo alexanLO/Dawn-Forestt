1ef8e8f63a06588b3515dcb19c0b48a4c0ed0ad2
Alexan Lima <90114869+alexanLO@users.noreply.github.com>
2024-04-13 21:57:15 -0300

    Atualizando Path dos itens

--- scripts/enemy/crabby/crabby.gd
+++ scripts/enemy/crabby/crabby.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	randomize()
 	drop_list = {
 		"Heal Potion": [
-			"res://assets/item/consumable/+1_health_potion.png",
+			"res://assets/item/consumable/health_potion.png",
 			15, #Chance de drop.
 			"Health", #Tipo do item.
 			5, #Valor que a poção recupera a vida.
@@ -14,7 +14,7 @@ func _ready() -> void:
 		],
 		
 		"Mana Potion": [
-			"res://assets/item/consumable/+1_mana_potion.png",
+			"res://assets/item/consumable/mana_potion.png",
 			8,
 			"Mana", 
 			5, 
