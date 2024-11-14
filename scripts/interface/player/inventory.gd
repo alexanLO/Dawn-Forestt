e7ff4af00e5536aeb3434c4c957db893ff689024
Alexan Lima <alexanlima1.0@gmail.com>
2024-11-13 21:41:58 -0300

    Script do Inventário - Configurando o Slot [Parte 1].

--- scripts/interface/player/inventory.gd
+++ scripts/interface/player/inventory.gd
@@ -14,6 +14,7 @@ var can_click: bool
 var is_visible: bool
 
 var item_index: int
+var item_quantity: int = 99
 
 #Essas 2 lista vai ser usada uma para popular os itens e a outra vai ser ultilizada para a serialização dos dados
 #Quando a gente chega na parte de serviço ou de dados que a gente vai e#star salvando e carregando as
@@ -32,13 +33,29 @@ var slot_list: Array = [
 	"", "", "", "", "", "", "", 
 ]
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for children in slot_container.get_children():
 		children.connect("empty_slot", Callable(self, "empty_slot"))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func update_slot(item_name: String, item_image: CompressedTexture2D, item_info: Array) -> void:
+	var existing_item_index: int = slot_list.find(item_name)
+	if existing_item_index != -1:
+		var item_slot: TextureRect = slot_container.get_child(existing_item_index)
+		if item_slot.amount < item_quantity and item_slot.item_type != "Equipament" and item_slot.item_type != "Weapon":
+			var current_amount: int = item_slot.amount + item_info[4]
+			if current_amount > item_quantity:
+				var leftover: int = current_amount - item_quantity
+				item_info[3] = item_quantity - item_slot.amount
+				item_slot.update_item(item_name, item_image, item_info)
+				item_info[3] = leftover
+				update_slot(item_name, item_image, item_info)
+				return
+			item_slot.update_item(item_name, item_image, item_info)
+			return
+			
+		
+	
+
 func empty_slot(index: int) -> void:
 	slot_list[index] = ""
 	slot_item_info[index] = ""
